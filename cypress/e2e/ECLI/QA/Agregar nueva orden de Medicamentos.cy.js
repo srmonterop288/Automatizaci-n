3559600b555c2df0b79ac88260d2921d5fbe28e4
EfrenVaqueroLugo <evaquero-ext@ultratech-inc.com>
2025-06-17 13:21:09 -0600

    Actualización de Efrén

--- cypress/e2e/ECLI/QA/Agregar nueva orden de Medicamentos.cy.js
+++ cypress/e2e/ECLI/QA/Agregar nueva orden de Medicamentos.cy.js
@@ -1,50 +1,67 @@
 import { seleccionarAgenda } from "../../../funciones/ECLI/agenda";
-import { agregarDiagnosticoDeSalida, agregarMotivoDeConsultra } from "../../../funciones/ECLI/evaluacion";
+import {
+  agregarDiagnosticoDeSalida,
+  agregarMotivoDeConsultra,
+} from "../../../funciones/ECLI/evaluacion";
 import { finalizarConsulta } from "../../../funciones/ECLI/Flujos";
-import { iniciarUnaConsulta, loginECLI, obtenerIdoneaDelPaciente } from "../../../funciones/ECLI/funciones";
+import {
+  iniciarUnaConsulta,
+  loginECLI,
+  obtenerIdoneaDelPaciente,
+} from "../../../funciones/ECLI/funciones";
 import { inciarConsulta } from "../../../funciones/ECLI/agenda";
 import { agregarOrdenesDeMedicamentos } from "../../../funciones/ECLI/ordenes";
 
-describe('template spec', () => {
-  it('Agregar una nueva orden de Medicamentos', () => {
+describe("template spec", () => {
+  it("Agregar una nueva orden de Medicamentos", () => {
     let numero = 0;
     let botonMasNuevo = 0;
 
     loginECLI("QA");
 
-    cy.xpath('//*[@id="div_nombre_usuario"]/div/span[2]').invoke('text').then((text) => {
-      const tipoRol = text.trim();
-      seleccionarAgenda(tipoRol);
+    cy.xpath('//*[@id="div_nombre_usuario"]/div/span[2]')
+      .invoke("text")
+      .then((text) => {
+        const tipoRol = text.trim();
+        seleccionarAgenda(tipoRol);
 
-      iniciarUnaConsulta(tipoRol).then(({ numero: num, botonMasNuevo: boton, tipoBoton: cadena, tipoRol }) => {
-        numero = num;
-        botonMasNuevo = boton;
-        const tipoBoton = cadena;
+        iniciarUnaConsulta(tipoRol).then(
+          ({
+            numero: num,
+            botonMasNuevo: boton,
+            tipoBoton: cadena,
+            tipoRol,
+          }) => {
+            numero = num;
+            botonMasNuevo = boton;
+            const tipoBoton = cadena;
 
-        if (!numero && !botonMasNuevo && !tipoBoton) {
-          cy.log('NO SE ENCONTRARON CONSULTAS DISPONIBLES EN LA AGENDA SELECCIONADA');
-        } else if (!botonMasNuevo) {
-          cy.log('No se encontró ningún botón para la agenda.');
-        } else {
-          inciarConsulta(numero, botonMasNuevo, tipoBoton, tipoRol)
-          
+            if (!numero && !botonMasNuevo && !tipoBoton) {
+              cy.log(
+                "NO SE ENCONTRARON CONSULTAS DISPONIBLES EN LA AGENDA SELECCIONADA"
+              );
+            } else if (!botonMasNuevo) {
+              cy.log("No se encontró ningún botón para la agenda.");
+            } else {
+              inciarConsulta(numero, botonMasNuevo, tipoBoton, tipoRol);
 
-          //AUÍ AGREGAMOS EL PACIENTE ATENDIDO
-          obtenerIdoneaDelPaciente()
+              //AQUÍ AGREGAMOS EL PACIENTE ATENDIDO
+              obtenerIdoneaDelPaciente();
 
-          //AQUÍ ABAJO AGREGAMOS LO MÓDULOS
-          agregarOrdenesDeMedicamentos(tipoRol)
-          cy.readFile('cypress/fixtures/numeroDeReceta.json').then((data) => {
-              const receta = data.valor;
+              //AQUÍ ABAJO AGREGAMOS LO MÓDULOS
+              agregarOrdenesDeMedicamentos(tipoRol);
+              cy.readFile("cypress/fixtures/numeroDeReceta.json").then(
+                (data) => {
+                  const receta = data.valor;
 
-              // Usamos el valor
-              cy.log('Leemos el campo de receta: '+receta)
-              //return receta;
-          });
-          
-        }
+                  // Usamos el valor
+                  cy.log("Leemos el campo de receta: " + receta);
+                  //return receta;
+                }
+              );
+            }
+          }
+        );
       });
-    });
   });
-})
-
+});
