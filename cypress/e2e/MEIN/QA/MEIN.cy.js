1135e43ac6360e2a6522b5c350dd55e604844ae5
EfrenVaqueroLugo <evaquero-ext@ultratech-inc.com>
2025-06-18 10:34:59 -0600

    Borrado de prueba de rama

--- cypress/e2e/MEIN/QA/MEIN.cy.js
+++ cypress/e2e/MEIN/QA/MEIN.cy.js
@@ -1,87 +1,121 @@
-require('cypress-xpath')
-import 'cypress-iframe';
+require("cypress-xpath");
+import "cypress-iframe";
 
 import {
-  loginECLI, iniciarUnaConsulta, opcionesNuevaConsulta, personaNoAsistio, pacienteNoAtendido,
-  loginECLISTG, loginECLIQA,
+  loginECLI,
+  iniciarUnaConsulta,
+  opcionesNuevaConsulta,
+  personaNoAsistio,
+  pacienteNoAtendido,
+  loginECLISTG,
+  loginECLIQA,
   loginECLICAP,
-  obtenerIdoneaDelPaciente
-} from '../../../funciones/ECLI/funciones';
-import { inciarConsulta, seleccionarAgenda, servicio } from '../../../funciones/ECLI/agenda';
-import { agregarOrdenesDeMedicamentos, obtenerValorReceta, seleccionarOrdenes } from '../../../funciones/ECLI/ordenes';
-import { loginMEIN, loginMEINQA } from '../../../funciones/MEIN/loginMEIN';
-import { bloquearPDF } from '../../../funciones/MEIN/cerrarPDF';
-import { dispensacionMedicamento, dispensacionMedicamentos, entregarMedicamento, preparacionMedicamento, verificarReceta, verificarRecetaSimple } from '../../../funciones/MEIN/funciones';
+  obtenerIdoneaDelPaciente,
+} from "../../../funciones/ECLI/funciones";
+import {
+  inciarConsulta,
+  seleccionarAgenda,
+  servicio,
+} from "../../../funciones/ECLI/agenda";
+import {
+  agregarOrdenesDeMedicamentos,
+  obtenerValorReceta,
+  seleccionarOrdenes,
+} from "../../../funciones/ECLI/ordenes";
+import { loginMEIN, loginMEINQA } from "../../../funciones/MEIN/loginMEIN";
+import { bloquearPDF } from "../../../funciones/MEIN/cerrarPDF";
+import {
+  dispensacionMedicamento,
+  dispensacionMedicamentos,
+  entregarMedicamento,
+  preparacionMedicamento,
+  verificarReceta,
+  verificarRecetaSimple,
+} from "../../../funciones/MEIN/funciones";
 
-describe('Medicamentos e insumos', () => {
-   // Ignora errores específicos del ResizeObserver
-  Cypress.on('uncaught:exception', (err, runnable) => {
-    if (err.message.includes('ResizeObserver loop completed with undelivered notifications')) {
+describe("Medicamentos e insumos", () => {
+  // Ignora errores específicos del ResizeObserver
+  Cypress.on("uncaught:exception", (err, runnable) => {
+    if (
+      err.message.includes(
+        "ResizeObserver loop completed with undelivered notifications"
+      )
+    ) {
       return false;
     }
   });
 
-  it('Realizar nueva orden de medicamento', () => {
+  it("Realizar nueva orden de medicamento", () => {
     let numero = 0;
     let botonMasNuevo = 0;
 
     loginECLI("QA");
 
-    cy.xpath('//*[@id="div_nombre_usuario"]/div/span[2]').invoke('text').then((text) => {
-      const tipoRol = text.trim();
-      seleccionarAgenda(tipoRol);
+    cy.xpath('//*[@id="div_nombre_usuario"]/div/span[2]')
+      .invoke("text")
+      .then((text) => {
+        const tipoRol = text.trim();
+        seleccionarAgenda(tipoRol);
 
-      iniciarUnaConsulta(tipoRol).then(({ numero: num, botonMasNuevo: boton, tipoBoton: cadena, tipoRol }) => {
-        numero = num;
-        botonMasNuevo = boton;
-        const tipoBoton = cadena;
+        iniciarUnaConsulta(tipoRol).then(
+          ({
+            numero: num,
+            botonMasNuevo: boton,
+            tipoBoton: cadena,
+            tipoRol,
+          }) => {
+            numero = num;
+            botonMasNuevo = boton;
+            const tipoBoton = cadena;
 
-        if (!numero && !botonMasNuevo && !tipoBoton) {
-          cy.log('NO SE ENCONTRARON CONSULTAS DISPONIBLES EN LA AGENDA SELECCIONADA');
-        } else if (!botonMasNuevo) {
-          cy.log('No se encontró ningún botón para la agenda.');
-        } else {
-          inciarConsulta(numero, botonMasNuevo, tipoBoton, tipoRol);
+            if (!numero && !botonMasNuevo && !tipoBoton) {
+              cy.log(
+                "NO SE ENCONTRARON CONSULTAS DISPONIBLES EN LA AGENDA SELECCIONADA"
+              );
+            } else if (!botonMasNuevo) {
+              cy.log("No se encontró ningún botón para la agenda.");
+            } else {
+              inciarConsulta(numero, botonMasNuevo, tipoBoton, tipoRol);
 
-          //AQUÍ AGREGAMOS EL PACIENTE ATENDIDO
-          obtenerIdoneaDelPaciente()
+              //AQUÍ AGREGAMOS EL PACIENTE ATENDIDO
+              obtenerIdoneaDelPaciente();
 
-          //AQUÍ ABAJO AGREGAMOS LO MÓDULOS
-          agregarOrdenesDeMedicamentos(tipoRol)
-          cy.readFile('cypress/fixtures/numeroDeReceta.json').then((data) => {
-              const receta = data.valor;
+              //AQUÍ ABAJO AGREGAMOS LO MÓDULOS
+              agregarOrdenesDeMedicamentos(tipoRol);
+              cy.readFile("cypress/fixtures/numeroDeReceta.json").then(
+                (data) => {
+                  const receta = data.valor;
 
-              //Usamos el valor
-              cy.log('Leemos el campo de receta: '+receta)
-              //return receta;
-          });
-          //Prueba de RAMA
-        }
+                  //Usamos el valor
+                  cy.log("Leemos el campo de receta: " + receta);
+                  //return receta;
+                }
+              );
+            }
+          }
+        );
       });
-    });
   });
 
-  it('Pruebas', () => {
+  it("Pruebas", () => {
     // ✅ Bloquear la descarga del PDF si se hace por red
 
-    loginMEIN("QA")
-    cy.readFile('cypress/fixtures/numeroDeReceta.json').then((data) => {
+    loginMEIN("QA");
+    cy.readFile("cypress/fixtures/numeroDeReceta.json").then((data) => {
       const idoneaDelPaciente = data.idoneaDelPaciente;
       const receta = data.valor;
-      
+
       //Usamos el valor
-      if(idoneaDelPaciente != "" && receta != ""){
-        verificarReceta(idoneaDelPaciente, receta)
-        preparacionMedicamento(receta)
-        dispensacionMedicamento(receta)
-        entregarMedicamento(receta)
-      }
-      else{
-        cy.log("Idonea médica o Número de Receta VACIOS")
+      if (idoneaDelPaciente != "" && receta != "") {
+        verificarReceta(idoneaDelPaciente, receta);
+        preparacionMedicamento(receta);
+        dispensacionMedicamento(receta);
+        entregarMedicamento(receta);
+      } else {
+        cy.log("Idonea médica o Número de Receta VACIOS");
       }
       //cy.log('Leemos el campo de receta: '+receta)
       //return receta;
     });
-  })
-
+  });
 });
